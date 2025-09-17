854892f8815e0494d8d84d1df894249ce8d59d37
Hedwig0625 <hedwigchen060625@gmail.com>
2025-09-17 09:27:28 +0800

    Update main4.cpp

--- hw1/main4.cpp
+++ hw1/main4.cpp
@@ -1,13 +1,26 @@
-#include <iomanip>
+#include <string>
+#include <vector>
 
-void assignment2_part2(int x, size_t y, char** names) {
-    std::cout << "\n對齊後的學生名單:" << std::endl;
+void assignment2_part1(int& x, size_t& y, char**& names) {
+    std::cout << "請輸入學生人數x: ";
+    std::cin >> x;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    std::vector<std::string> temp_names;
+    y = 0;
+
+    std::cout << "請依序輸入 " << x << " 位學生的英文全名:" << std::endl;
     for (int i = 0; i < x; ++i) {
-        std::cout << std::left << std::setw(y) << names[i]
-                  << " (長度: " << strlen(names[i]) << ")" << std::endl;
+        std::string name;
+        std::cout << "學生 " << i + 1 << ": ";
+        std::getline(std::cin, name);
+        temp_names.push_back(name);
+        if (name.length() > y) y = name.length();
     }
 
-    for (int i = 0; i < x; ++i) delete[] names[i];
-    delete[] names;
-    std::cout << "\n已釋放動態陣列記憶體。\n";
+    names = new char*[x];
+    for (int i = 0; i < x; ++i) {
+        names[i] = new char[y + 1];
+        strcpy(names[i], temp_names[i].c_str());
+    }
 }
