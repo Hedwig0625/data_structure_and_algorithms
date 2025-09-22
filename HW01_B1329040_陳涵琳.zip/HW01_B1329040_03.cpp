6761b0f2fbdc5071b1880514279d505a1b42f615
Hedwig0625 <hedwigchen060625@gmail.com>
2025-09-22 21:27:23 +0800

    Update HW01_B1329040_03.cpp

--- HW01_B1329040_陳涵琳.zip/HW01_B1329040_03.cpp
+++ HW01_B1329040_陳涵琳.zip/HW01_B1329040_03.cpp
@@ -1,4 +1,4 @@
-#include <string>
+#include <cstring>
 #include <iostream>
 #include <limits>
 using namespace std;
@@ -8,8 +8,8 @@ void bubble(char** names, int n) {
       
         for (int j = 0; j < n - i - 1; ++j) {
           
-            int len1 = strlen(names[j]);
-            int len2 = strlen(names[j+1]);
+            int len1 = strlen (names[j]);
+            int len2 = strlen (names[j+1]);
           
             if (len1 > 0 && len2 > 0 && names[j][len1 - 1] > names[j + 1][len2 - 1]) {
               
@@ -22,6 +22,39 @@ void bubble(char** names, int n) {
     }
 }
 
+int first_name(char* str){
+    char* p = strchr(str,' ');
+    if(p == nullptr){
+        return strlen(str);
+    }else{
+        return p - str;
+    }
+}
+
+void space_sort(char** names,int n){
+    
+    int max =0;
+    
+    for(int k=0;k <n-1;++k){
+        int x= first_name (names[k]);
+        if(x > max){
+            max =x;
+        }
+    }
+
+    for(int m=0;m<n;++m){
+        int x = first_name(names[m]);
+        int space =max -x;
+
+        for(int o=0;o< space;++o){
+            cout <<" ";
+        }
+    cout << names[m] << endl;
+    }
+    
+    
+}
+
 int main(){
     int n;
     cout << "請輸入學生人數n: ";
@@ -31,19 +64,23 @@ int main(){
 
     char** names= new char*[n];
     
+    int max= 101;
+    
     for (int i = 0; i < n; ++i) {
 
-        names[i] = new char[m+1];
+        names[i] = new char[max];
         
-        cout <<"請輸入 第" << i+1 << "位學生的姓名 (長度不超過 " << m << " 個字元):" <<endl;
-        cin.getline(names[i],m+1);
+        cout <<"請輸入 第" << i+1 << "位學生的姓名" <<endl;
+        
+        cin.getline(names[i],max);
         
     }
 
     bubble(names,n);
     
-    
     cout << "輸入的學生姓名如下:"<<endl;
+    
+    space_sort(names,n);
     
     for(int l=0;l<n;++l){
         delete[] names[l];
